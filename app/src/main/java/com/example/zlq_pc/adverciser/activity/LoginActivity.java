636ba263237ce3c5ae9f70038179e14d3a929d41
U <1539753763@qq.com>
2018-01-31 23:06:31 +0800

    --。

--- app/src/main/java/com/example/zlq_pc/adverciser/activity/LoginActivity.java
+++ app/src/main/java/com/example/zlq_pc/adverciser/activity/LoginActivity.java
@@ -1,22 +1,24 @@
 package com.example.zlq_pc.adverciser.activity;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.zlq_pc.adverciser.R;
 import com.ht.uilib.base.BaseActivity;
 
 public class LoginActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+    private TextView tvLineLogin,tvLineRegister,tvRegister,tvLogin;
+    private LinearLayout llLogin,llRegisterrr;
 
     @Override
     protected void initTitleBar() {
-        mTitleBarView.setTitleText("登陆");
+        mTitleBarView.setTitleText("登录").setTitleViewVisibility(View.VISIBLE);
     }
 
     @Override
@@ -31,6 +33,12 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initChildView() {
+        tvLineLogin =(TextView) findViewById(R.id.tv_line_login);
+        tvLineRegister =(TextView) findViewById(R.id.tv_line_registerr);
+        tvRegister =(TextView) findViewById(R.id.tv_register);
+        tvLogin =(TextView) findViewById(R.id.tv_login);
+        llLogin = (LinearLayout) findViewById(R.id.ll_login);
+        llRegisterrr = (LinearLayout) findViewById(R.id.ll_register);
 
     }
 
@@ -41,6 +49,24 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void setChildViewListener() {
+        llRegisterrr.setOnClickListener(this);
+        llLogin.setOnClickListener(this);
+    }
 
+    @SuppressLint("ResourceAsColor")
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()){
+            case R.id.ll_login:
+                tvLineRegister.setBackground(getResources().getDrawable(R.color.white));
+                tvLineLogin.setBackground(getResources().getDrawable(R.color.blue_btn_press));
+                break;
+            case R.id.ll_register:
+                tvLineRegister.setBackground(getResources().getDrawable(R.color.blue_btn_press));
+                tvLineLogin.setBackground(getResources().getDrawable(R.color.white));
+                break;
+        }
     }
 }
